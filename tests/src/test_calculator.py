13802dc86562c3f1d78ef10b9110672d9b18e768
Błażej Łach <lachuu95@gmail.com>
2019-11-08 22:16:15 +0100

    dodanie wszytkich testów

--- tests/src/test_calculator.py
+++ tests/src/test_calculator.py
@@ -12,7 +12,7 @@ def test_calculator():
     assert True
 
 
-@pytest.mark.parametrize("test_input_a", [None,"qwerty", 1, 1.15, True])
+@pytest.mark.parametrize("test_input_a", [None, "qwerty", 1, 1.15, True])
 def test_calculator_wrong(test_input_a):
     with pytest.raises(TypeError):
         Calculator(test_input_a)
@@ -57,44 +57,16 @@ def test_calculator_addition(test_input_a, test_input_b, expected, get_class_obj
 
 @pytest.mark.parametrize(
     "test_input_a, test_input_b, expected",
-    [
-        (0, 0, 0),
-        (10, -25, -15),
-        (-2, -3, -5),
-        (5, 3, 8),
-        (1585416574651, 468516504186, 2053933078837),
-        (
-            -3524653413534854165135468848486,
-            -3598798768489748658746846879854,
-            -7123452182024602823882315728340,
-        ),
-        (0.0, 0.0, 0.0),
-        (1.52, 3.241, 4.761),
-        (-2.01, -0.365, -2.375),
-        (3.25, -1.25, 2),
-        (
-            0.000000000000000000000000000000000003,
-            0.000000000000000000000000000000000001,
-            0.000000000000000000000000000000000004,
-        ),
-        (3, 1.5, 4.5),
-        (True, True, 2),
-    ],
+    [("A", 1, "A1"), (2, b"1", 3), (None, 1, 1), (2 + 3j, 3 + 2j, 5 + 5j),],
 )
-def test_calculator_addition(test_input_a, test_input_b, expected, get_class_object):
-    assert isclose(
-        get_class_object.addition(test_input_a, test_input_b), expected, abs_tol=1e-10,
-    )
-
-
-@pytest.mark.parametrize(
-    "test_input_a, test_input_b, expected",
-    [("A", 1, "A1"), (2, b"1", 3), (None, 1, 1), (2+3j, 3+2j, 5+5j), ],
-)
-def test_calculator_addition_wrong(test_input_a, test_input_b, expected, get_class_object):
+def test_calculator_addition_wrong(
+    test_input_a, test_input_b, expected, get_class_object
+):
     with pytest.raises(TypeError):
         assert isclose(
-            get_class_object.addition(test_input_a, test_input_b), expected, abs_tol=1e-10,
+            get_class_object.addition(test_input_a, test_input_b),
+            expected,
+            abs_tol=1e-10,
         )
 
 
@@ -121,6 +93,7 @@ def test_calculator_addition_wrong(test_input_a, test_input_b, expected, get_cla
             0.000000000000000000000000000000000002,
         ),
         (3, 1.5, 1.5),
+        (True, True, 0),
     ],
 )
 def test_calculator_subtraction(test_input_a, test_input_b, expected, get_class_object):
@@ -133,12 +106,16 @@ def test_calculator_subtraction(test_input_a, test_input_b, expected, get_class_
 
 @pytest.mark.parametrize(
     "test_input_a, test_input_b, expected",
-    [("A", 1, "A-1"), (2, b"1", 1), (None, 1, -1), (2+3j, 3+2j, -1+1j), ],
+    [("A", 1, "A-1"), (2, b"1", 1), (None, 1, -1), (2 + 3j, 3 + 2j, -1 + 1j),],
 )
-def test_calculator_subtraction_wrong(test_input_a, test_input_b, expected, get_class_object):
+def test_calculator_subtraction_wrong(
+    test_input_a, test_input_b, expected, get_class_object
+):
     with pytest.raises(TypeError):
         assert isclose(
-            get_class_object.subtraction(test_input_a, test_input_b), expected, abs_tol=1e-10,
+            get_class_object.subtraction(test_input_a, test_input_b),
+            expected,
+            abs_tol=1e-10,
         )
 
 
@@ -165,6 +142,7 @@ def test_calculator_subtraction_wrong(test_input_a, test_input_b, expected, get_
             3e-72,
         ),
         (3, 1.5, 4.5),
+        (True, True, 1),
     ],
 )
 def test_calculator_multiplication(
@@ -175,6 +153,21 @@ def test_calculator_multiplication(
         expected,
         abs_tol=1e-10,
     )
+
+
+@pytest.mark.parametrize(
+    "test_input_a, test_input_b, expected",
+    [("A", 1, "A*1"), (2, b"1", 2), (None, 1, 0), (2 + 3j, 3 + 2j, 13j),],
+)
+def test_calculator_multiplication_wrong(
+    test_input_a, test_input_b, expected, get_class_object
+):
+    with pytest.raises(TypeError):
+        assert isclose(
+            get_class_object.multiplication(test_input_a, test_input_b),
+            expected,
+            abs_tol=1e-10,
+        )
 
 
 @pytest.mark.parametrize(
@@ -198,6 +191,7 @@ def test_calculator_multiplication(
             3,
         ),
         (3, 1.5, 2),
+        (True, True, 1),
     ],
 )
 def test_calculator_division(test_input_a, test_input_b, expected, get_class_object):
@@ -207,11 +201,70 @@ def test_calculator_division(test_input_a, test_input_b, expected, get_class_obj
 
 
 @pytest.mark.parametrize(
+    "test_input_a, test_input_b, expected",
+    [
+        ("A", 1, "A/1"),
+        (2, b"1", 2),
+        (None, 1, 0),
+        (2 + 3j, 3 + 2j, 0.9230769230769231 + 0.38461538461538464j),
+    ],
+)
+def test_calculator_division_wrong_type(
+    test_input_a, test_input_b, expected, get_class_object
+):
+    with pytest.raises(TypeError):
+        assert isclose(
+            get_class_object.division(test_input_a, test_input_b),
+            expected,
+            abs_tol=1e-10,
+        )
+
+
+@pytest.mark.parametrize(
+    "test_input_a, test_input_b, expected", [(1, 0, 0), (1.1, 0.0, 0.1),],
+)
+def test_calculator_division_wrong_value(
+    test_input_a, test_input_b, expected, get_class_object
+):
+    with pytest.raises(ZeroDivisionError):
+        assert isclose(
+            get_class_object.division(test_input_a, test_input_b),
+            expected,
+            abs_tol=1e-10,
+        )
+
+
+@pytest.mark.parametrize(
     "test_input_a, expected",
-    [(0, 1), (1, 1), (2, 2), (3, 6), (10, 3628800), (20, 2432902008176640000),],
+    [
+        (0, 1),
+        (1, 1),
+        (2, 2),
+        (3, 6),
+        (10, 3628800),
+        (20, 2432902008176640000),
+        (True, 1),
+    ],
 )
 def test_calculator_factorial(test_input_a, expected, get_class_object):
     assert get_class_object.factorial(test_input_a) == expected
+
+
+@pytest.mark.parametrize(
+    "test_input_a, expected",
+    [(2.563, 5.126), ("A", "A"), ("B", "AB"), (None, 1), (2 + 3j, 2 + 6j), (b"3", 6)],
+)
+def test_calculator_factorial_wrong_type(test_input_a, expected, get_class_object):
+    with pytest.raises(TypeError):
+        assert get_class_object.factorial(test_input_a) == expected
+
+
+@pytest.mark.parametrize(
+    "test_input_a, expected", [(-1, 1), (-3, -6),],
+)
+def test_calculator_factorial_wrong_value(test_input_a, expected, get_class_object):
+    with pytest.raises(ValueError):
+        assert get_class_object.factorial(test_input_a) == expected
 
 
 @pytest.mark.parametrize(
@@ -223,7 +276,25 @@ def test_calculator_factorial(test_input_a, expected, get_class_object):
         (3, 1.73205080756),
         (10, 3.16227766016),
         (20, 4.47213595499),
+        (True, 1),
     ],
 )
 def test_calculator_square_root(test_input_a, expected, get_class_object):
     assert isclose(get_class_object.square_root(test_input_a), expected, abs_tol=1e-10)
+
+
+@pytest.mark.parametrize(
+    "test_input_a, expected",
+    [("A", "sqrt(A)"), (None, 0), (4 + 9j, 2 + 3j), (b"4", 2)],
+)
+def test_calculator_square_root_wrong_type(test_input_a, expected, get_class_object):
+    with pytest.raises(TypeError):
+        assert get_class_object.square_root(test_input_a) == expected
+
+
+@pytest.mark.parametrize(
+    "test_input_a, expected", [(-1, 1j), (-9, 3j),],
+)
+def test_calculator_square_root_wrong_value(test_input_a, expected, get_class_object):
+    with pytest.raises(ValueError):
+        assert get_class_object.square_root(test_input_a) == expected
